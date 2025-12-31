d36af201a937f6168150dfe9be599c72fef17e71
SSR <someshwar.sangaraju19@gmail.com>
2025-12-31 21:07:19 +0530

    eks cluster

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "someshwar-devops"
-    key    = "eks-vpc-module"
+    key    = "eks-vpc-sg"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
